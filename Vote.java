8e302f542d81dbe19197572f693006f6fa44d833
OscarHealyATU <g00424324@atu.ie>
2022-11-29 13:34:05 +0000

    new file:   Vote.java

---
+++ Vote.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Vote {
+    public static int AOC = 18;
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = input.nextInt();
+
+        if (age < AOC) {
+            System.out.println("not eligible to vote");
+        } else {
+            System.out.println("eligible to vote");
+        }
+
+    }
+}
