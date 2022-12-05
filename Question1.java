01a5f99378c5001cac6820aadc65d4d06f09128e
OscarHealyATU <g00424324@atu.ie>
2022-12-05 11:11:02 +0000

    new file:   Question1.java

---
+++ Question1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Question1
+ */
+public class Question1 {
+
+    public static void main(String[] args) {
+        //variables
+        boolean q1 = false;
+        int count = 0;
+        String ans;
+
+        // input code
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nThe following code produces which output to the screen:\n\n");
+        System.out.println(" int x = 65;\n if(x > 65){\n\tSystem.out.println(x);\n}");
+        System.out.println("\n\n is it \nA.|\t65 \nB.|\tNothing is printed to the screen");
+        ans = input.next();
+
+        // Answer decision
+        if (ans.equals("a")) {
+            q1 = true;
+            System.out.println("correct!");
+        }else {
+            q1 = false;
+            System.out.println("correct!");
+        }
+       
+    }
+}
