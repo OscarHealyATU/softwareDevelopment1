ec178beeee995b0f5aac36fe64606373b070f718
OscarHealyATU <g00424324@atu.ie>
2022-12-20 13:56:20 +0000

    new file:   christmasExam/Exercise1.java 	new file:   christmasExam/Exercise2.java 	new file:   christmasExam/Exercise3.java 	new file:   christmasExam/Exercise4.java

---
+++ christmasExam/Exercise1.java
@@ -0,0 +1,12 @@
+package christmasExam;
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*********************");
+        System.out.println("\t*****");
+        System.out.println("\t*****");
+        System.out.println("\t*****");
+        
+        System.out.println("*********************");
+    }
+}
