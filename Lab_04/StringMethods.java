3368f329c116230ca9c4d73fb23a1863703258ac
OscarHealyATU <g00424324@atu.ie>
2023-01-13 15:13:52 +0000

    modified:   Lab_04/LineNumber.java 	modified:   Lab_04/StringMethods.java

--- Lab_04/StringMethods.java
+++ Lab_04/StringMethods.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class StringMethods {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
         String word;
         int lengthGuess;
+
         System.out.println("type a word in");
         word = input.next();
+
         lengthGuess = word.length();
         System.out.println("word length is " + lengthGuess);
+        
         input.close();
     }
 }
