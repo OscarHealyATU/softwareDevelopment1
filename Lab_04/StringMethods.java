fc484dce79cbbb130f63b865e98d06ff7ed59f9e
OscarHealyATU <g00424324@atu.ie>
2023-01-16 00:45:44 +0000

    modified:   Lab_04/StringMethods.java

--- Lab_04/StringMethods.java
+++ Lab_04/StringMethods.java
@@ -5,13 +5,23 @@ public class StringMethods {
         
         Scanner input = new Scanner(System.in);
         String word;
+        char guess;
         int lengthGuess;
 
         System.out.println("type a word in");
         word = input.next();
 
-        lengthGuess = word.length();
-        System.out.println("word length is " + lengthGuess);
+        System.out.println("word length is " + word.length());
+
+        System.out.println(word.charAt(0));
+        System.out.println(word.charAt(word.length()-1));
+
+        System.out.println(word.indexOf(word.charAt(1)));
+        System.out.println("enter a letter in the word");
+        input.next();
+        
+        
+        
         
         input.close();
     }
