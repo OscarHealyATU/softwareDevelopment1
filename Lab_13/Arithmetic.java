b3a321ee980f74ae0b026408932059845dcf3ccb
OscarHealyATU <g00424324@atu.ie>
2023-01-23 13:09:01 +0000

    modified:   Lab_13/Arithmetic.java

--- Lab_13/Arithmetic.java
+++ Lab_13/Arithmetic.java
@@ -12,12 +12,12 @@ public class Arithmetic {
             d = input.nextInt(), 
             e = input.nextInt();
 
-            System.out.println("Value of a, "+a+", after adding "+(a+=9));
-            System.out.println("Value of a, "+b+", after subtracting "+(b-=4));
-            System.out.println("Value of a, "+c+", after multiplying "+(c*=3));
-            System.out.println("Value of a, "+d+", after dividing "+(d/=2));
-            System.out.println("Remainder value of assigned to e, "+e+", after dividing e by 5: "+(e%=5));
-       
-            input.close();
+        System.out.println("Value of a, " + a + ", after adding " + (a+=9));
+        System.out.println("Value of a, " + b + ", after subtracting " + (b-=4));
+        System.out.println("Value of a, " + c + ", after multiplying " + (c*=3));
+        System.out.println("Value of a, " + d + ", after dividing " + (d/=2));
+        System.out.println("Remainder value of assigned to e, " + e + ", after dividing e by 5: " + (e%=5));
+    
+        input.close();
     }
 }
