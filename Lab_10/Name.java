4641485c355aa698778ea4f5191833872639b5e7
OscarHealyATU <g00424324@atu.ie>
2023-01-26 08:34:41 +0000

    modified:   Lab_10/Name.java

--- Lab_10/Name.java
+++ Lab_10/Name.java
@@ -1,3 +1,4 @@
+package Lab_10;
 import java.util.Scanner;
 
 /**
@@ -10,12 +11,12 @@ public class Name {
         System.out.println("Enter a name and we'll check if its the same as mine");
         String myName = "Oscar",
                nameStr = input.next();
-              
+        input.close();
         System.out.print("\nName entered is " + nameStr + " ");
         if (nameStr.equals(myName)) {
             System.out.println("it is the same as " + myName);
         } 
        System.out.println("it is not the same as " + myName);
-        input.close();
+       
     }
 }
