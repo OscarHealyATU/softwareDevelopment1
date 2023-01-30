b0256952b743a1ac8b35c94521fd1790746c4ee2
OscarHealyATU <g00424324@atu.ie>
2023-01-30 08:44:54 +0000

    new file:   Age.java 	new file:   Swap.java

---
+++ Age.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+        int age;
+
+        System.out.println("Enter your name.");
+        name = input.next();
+
+        System.out.println("Enter your age.");
+        age = input.nextInt();
+        input.close();
+        System.out.print("\nHi " + name + ", ");
+        if (age < 66) {
+            if (age < 4) {
+                System.out.print("you are a toddler. ");
+            }
+            if(age >= 3 && age <= 13){
+                System.out.print("you are a child. ");
+            }
+            if (age > 12 && age < 20) {
+                System.out.print("you are a teenager. ");
+            }
+            if (age >19 && age < 66) {
+                System.out.print("you are an adult. ");
+            }
+         }else {
+        System.out.print("you are a senior citezen ");
+        }        
+        System.out.println("");
+    }
+}
---
+++ Swap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String value for a.");
+        String a = input.next();
+        System.out.println("Enter String value for b.");
+        String b = input.next();
+        System.out.println("Strings before swap a = " + a + "and b = " + b);
+
+    }
+}
