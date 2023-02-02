7e619deac9f5c46633eb00c7dd22bfab715a6ef9
OscarHealyATU <g00424324@atu.ie>
2023-02-02 15:55:07 +0000

    modified:   Semester_01/Lab_06/GeometryApp.java

--- Semester_01/Lab_06/GeometryApp.java
+++ Semester_01/Lab_06/GeometryApp.java
@@ -3,25 +3,36 @@ import java.util.Scanner;
 public class GeometryApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int length, area, perim,height,radius,circDouble, diameter;
         double circ;
-
+        int length,
+            width,
+            area, 
+            perim,
+            height,
+            radius,
+            circDouble, 
+            diameter;
+        
         System.out.println("Welcome to Geometry App!");
         System.out.println("Use this app to calculate the area of Rectangles, Circles, Triangles, and Squares.");
-        System.out.print("enter the length of the rectangle: ");
-    
+        
+        System.out.print("\nEnter the length of the rectangle: ");
         length = input.nextInt();
-        area = length*length;
-        perim = length*4;
-        System.out.println("Area of square: " + area);
-        System.out.println("Perimeter of square is: " + perim);
-        System.out.print("enter  single digit for length and width: ");
+        System.out.print("Enter the width of the rectangle: ");
+        width = input.nextInt();
+
+        area = length*width;
+        System.out.println("Area of the rectangle: " + area);
+
+        System.out.print("enter the radius of the circle: ");
         radius = input.nextInt();     
+        
         circDouble = (int) (Math.PI*20*radius);
         circ = (double)circDouble/10;
         diameter = radius*2;
         System.out.println("Diameter of circle: " + diameter);
         System.out.println("circumference of circle is: " + circ);
+
         System.out.print("enter length for base: ");
         length = input.nextInt();
         System.out.print("enter height: ");
