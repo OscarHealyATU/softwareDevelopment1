c7115e99ec27fde6e48b390b2f6e03707c9dd921
OscarHealyATU <g00424324@atu.ie>
2023-02-13 15:16:53 +0000

    modified:   Lab_16/SumFor.java

--- Lab_16/SumFor.java
+++ Lab_16/SumFor.java
@@ -3,24 +3,27 @@ import java.util.Scanner;
 public class SumFor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter num ");
         String fullNum = input.next();
         input.close();
+        
         String output = "";
         int total = 0;
         
         for (int i = 0; i < fullNum.length(); i++) {
         char digitChr = fullNum.charAt(i);
+
         if (i < fullNum.length()-1) {
             output += digitChr + " + ";
         } else {
             output += digitChr + " = ";
         }
-        // copied |Integer.parseInt| from internet
-         total += Integer.parseInt(String.valueOf(digitChr));          
         
+        // copied |Integer.parseInt| from internet
+         total += Integer.parseInt(String.valueOf(digitChr));                  
         }
-        System.out.println(output + total);
 
+        System.out.println(output + total);
     }
 }
