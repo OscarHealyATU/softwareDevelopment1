50af29b9bd3d9f6cb7ff4a6e4887cea9c9f3b13b
OscarHealyATU <g00424324@atu.ie>
2023-03-06 09:48:21 +0000

    modified:   CountTill.java

--- CountTill.java
+++ CountTill.java
@@ -3,49 +3,83 @@ import java.util.Scanner;
 public class CountTill {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double cent5, cent10, cent20, cent50;
-        double euro1, euro2, euro5, euro10, euro20, euro50, euro100;
+        double total=0;
+        double cent5 = 0, cent10 = 0, cent20= 0, cent50 = 0;
+        double euro1 = 0, euro2 = 0, euro5 = 0, euro10 = 0, euro20 = 0, euro50 = 0, euro100 = 0;
 
-        /* Get the number of coins of each denomination
+        // Get the number of coins of each denomination
         System.out.print("Enter 5 cent coins: ");
-        cent5 = input.nextDouble() * 0.05;
+        total += input.nextDouble() * 0.05;
         System.out.print("Enter 10 cent coins: ");
-        cent10 = input.nextDouble() * 0.10;
+        total += input.nextDouble() * 0.10;
         System.out.print("Enter 20 cent coins: ");
-        cent20 = input.nextDouble() * 0.20;
+        total += input.nextDouble() * 0.20;
         System.out.print("Enter 50 cent coins: ");
-        cent50 = input.nextDouble() * 0.50;
+        total += input.nextDouble() * 0.50;
         System.out.print("Enter 1 euro coins: ");
-        euro1 = input.nextDouble();
+        total += input.nextDouble();
         System.out.print("Enter 2 euro coins: ");
-        euro2 = input.nextDouble() * 2;
+        total += input.nextDouble() * 2;
         System.out.print("Enter 5 euro notes: ");
-        euro5 = input.nextDouble() * 5;
+        total += input.nextDouble() * 5;
         System.out.print("Enter 10 euro notes: ");
-        euro10 = input.nextDouble() * 10;
+        total += input.nextDouble() * 10;
         System.out.print("Enter 20 euro notes: ");
-        euro20 = input.nextDouble() * 20;
+        total += input.nextDouble() * 20;
         System.out.print("Enter 50 euro notes: ");
-        euro50 = input.nextDouble() * 50;
+        total += input.nextDouble() * 50;
         System.out.print("Enter 100 euro notes: ");
-        euro100 = input.nextDouble() * 100;
-        input.close();*/
-
-        // Calculate the total amount of money
-        double[] denAmounts = {cent5, cent10, cent20, cent50, euro1, euro2, euro5, euro10, euro20, euro50, euro100};
-        double[] floatAmount = { 2.5, 5, 10, 10, 25, 15, 30, 30, 60, 60, 50, 0};
-        double total = euro1 + euro2 + euro5 + euro10 + euro20 + euro50 + euro100 + cent5 + cent10 + cent20 + cent50;
+        total += input.nextDouble() * 100;
+        input.close();
+               
         // Calculate the amount of float
         double takings = total - 300;
+        double totalTaking = takings;
+        System.out.println(takings + " this is takings");
         while (takings >= 0) {
-              int i = 0;
-              while (denAmounts[i] >= 0) {
-                
+              if (takings > 100) {
+                takings -= 100;
+                euro100++;
+              } else if (takings > 50) {
+                takings -= 50;
+                euro50++;     
+              } else if (takings > 20) {
+                takings -= 20;
+                euro20++;
+              } else if (takings > 10) {
+                takings -= 10;
+                euro10++;
+              } else if (takings > 5) {
+                takings -= 5;
+                euro5++;
+              } else if (takings > 2) {
+                takings -= 2;
+                euro2++;
+              } else if (takings > 1) {
+                takings -= 1;
+                euro1++;
+              } else if (takings > 0.50) {
+                takings -= 0.50;
+                cent50++;
+              } else if (takings > 0.20) {
+                takings -= 0.20;
+                cent20++;
+              } else if (takings > 0.10) {
+                takings -= 0.10;
+                cent10++;
+              } else if (takings >= 0.05) {
+                takings = 0;
+                cent5++;
+                break;
               }
+
+              
         }
         // Output the results
         System.out.println("Total: " + total + " euros");
-        System.out.println("Takings: " + takings + " euros");
+        System.out.println("Takings: " + totalTaking + " euros");
         System.out.println("Take: ");
+        System.out.println(euro100 + "\n" + euro50 + "\n" + euro20 + "\n" + euro10 + "\n" + euro5 + "\n" 
+            + euro2 + "\n" + euro1 + "\n" + cent50 + "\n" + cent20 + "\n" + cent10+ "\n" + cent5 + "\n");
     }
 }
