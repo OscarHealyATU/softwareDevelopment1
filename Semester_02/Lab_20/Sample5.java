583cdaa8c46dbf85d5f7907687217708558e6a7e
OscarHealyATU <g00424324@atu.ie>
2023-03-13 12:59:00 +0000

    modified:   Semester_02/Lab_20/Sample5.java 	modified:   Semester_02/Lab_20/Sample5b.java

--- Semester_02/Lab_20/Sample5.java
+++ Semester_02/Lab_20/Sample5.java
@@ -1,10 +1,10 @@
 public class Sample5 {
     public static void main(String[] args) {
         
-        int row = 4, column = 0;
+        int row = 0, column = 4;
 
-        for (int i = 4; i > column; i--) {
-            for (int j = 1; j <= row; j++) {
+        for (int i = 4; i > row; i--) {
+            for (int j = 1; j <= column; j++) {
                 if (j < i) {
                     System.out.print("\t");
                 }
--- Semester_02/Lab_20/Sample5b.java
+++ Semester_02/Lab_20/Sample5b.java
@@ -1,15 +1,11 @@
 public class Sample5b {
-    public static void main(String[] args) {
-        
-        int row = 4, column = 0;
-
-        for (int i = 4; i > column; i--) {
-            for (int j = 1; j <= row; j++) {
+    public static void main(String[] args) {       
+        for (int i = 4; i > 0; i--) {
+            for (int j = 1; j <= 4; j++) {
                 if (j < i) System.out.print("\t");
                 else System.out.print(j + "\t");
             }
-            System.out.print("\n");
+            System.out.println();
         }
     }
 }
-
