3b17d3392ae1904169a0d3f41095a9a42d577685
OscarHealyATU <g00424324@atu.ie>
2023-03-22 12:14:44 +0000

    modified:   Semester_02/Lab_21/ShoppingList.java

--- Semester_02/Lab_21/ShoppingList.java
+++ Semester_02/Lab_21/ShoppingList.java
@@ -6,7 +6,7 @@ public class ShoppingList {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("How many items do you want to add:");
-        String[] items = new String[input.nextInt()];
+        String[] items = new String[input.nextInt()-1];
         
         for (int i = 0; i < items.length; i++) {
             System.out.print("Add item " + (i+1)+ " to the list: ");
