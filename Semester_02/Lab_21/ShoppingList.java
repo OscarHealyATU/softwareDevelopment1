18b4f43a0d959f55af42a3dfb57376ba0aea875a
OscarHealyATU <g00424324@atu.ie>
2023-03-22 10:48:06 +0000

    new file:   Semester_02/Lab_21/ShoppingList.java

---
+++ Semester_02/Lab_21/ShoppingList.java
@@ -0,0 +1,12 @@
+package Lab_21;
+
+import java.util.Scanner;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many items do you want to add:");
+        String[] items = new String[input.nextInt()];
+
+    }
+}
