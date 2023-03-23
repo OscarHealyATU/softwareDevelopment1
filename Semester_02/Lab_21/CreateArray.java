0ffa3d6d74675d3e74ac6605fdebf061a5243531
OscarHealyATU <g00424324@atu.ie>
2023-03-23 15:26:51 +0000

    modified:   Semester_02/Lab_21/CreateArray.java

--- Semester_02/Lab_21/CreateArray.java
+++ Semester_02/Lab_21/CreateArray.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class CreateArray {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
+
         System.out.println("int or double array?");
         System.out.print("Enter 1 for int and 2 for double array: ");
         int type = input.nextInt();
+
         System.out.print("how many values do you need to store in the array: ");
         int length = input.nextInt(); input.close();
+
         if (type == 1) {
+
             int[] values = new int[length]; 
             for (int i = 0; i < values.length; i++) {
                 System.out.print("Enter value"+(i+1)+": ");
@@ -21,7 +26,9 @@ public class CreateArray {
                 System.out.print(values[i]);
                 if (i != values.length -1)  System.out.print(", ");
             }
+
         } else if (type == 2) {
+
             double[] values = new double[length]; 
             for (int i = 0; i < values.length; i++) {
                 System.out.print("Enter value"+(i+1)+": ");
