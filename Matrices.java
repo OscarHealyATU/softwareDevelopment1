1c607713fe767be4ff5c3b7593ba83727fc4230f
OscarHealyATU <g00424324@atu.ie>
2023-03-28 12:03:33 +0000

    new file:   Matrices.java

---
+++ Matrices.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Matrices {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Define size of matrix array
+        System.out.print("Enter Matrix height: ");
+        int height = input.nextInt();
+        System.out.print("Enter Matrix width: ");
+        int width = input.nextInt();       
+
+        // solve
+        // determinant
+        // inverse
+        // transpose
+        // addition
+        // subtraction
+        // multiply
+
+        // matrix arrays
+        int[][] matrix = new int[height][width];
+        int[][] matrixResult = new int[height][width];
+        
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix.length; j++) {
+            System.out.print("enter a number: ");
+             matrix[i][j] = input.nextInt();       
+            }
+        }
+        // matrix output
+        System.out.println("\n\tOutput:\t.\n-----------");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print("[" + matrix[i][j] + "] ");
+            }
+            System.out.println();
+        }
+    }
+}
